db62e3bd58472b2ec6ab9bf9e291a43a67eefdfc
whitedmason <masonwhited@byui.edu>
2024-10-29 15:14:17 -0600

    back and forth communication

--- client.py
+++ client.py
@@ -1,22 +1,37 @@
 import socket
+import threading
+import os
 
 server_ip = "127.0.0.1"
 server_port = 7000
 server_address = (server_ip, server_port)
 
+def reader(sock):
+    while True:
+        try:
+            response = sock.recv(1024)
+            response = str(response, "UTF-8")
+            print()
+            print(response)
+            print()
+        except:
+            print("Client Disconnected")
+            os._exit(0)
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect(server_address)
     print(f"Connected to server: {server_address}")
+
+    thread = threading.Thread(target=reader, args=(sock,))
+    thread.start()
+
+    print("Enter your messages to send (exit to quit):")
     while True:
-        message = input("Enter your message to send (exit to quit): ")
+        message = input()
         if message == "exit":
             break
-        
+
         message = bytes(message, "UTF-8")
         sock.sendall(message)
-
-        response = sock.recv(1024)
-        response = str(response, "UTF-8")
-        print(response)
 
 print("Client closed")
--- server.py
+++ server.py
@@ -5,16 +5,25 @@ server_ip = "127.0.0.1"
 server_port = 7000
 server_address = (server_ip, server_port)
 
+clients = {} #Key = Connection, Value = Address
+
+def broadcast(message, connection):
+    for client in clients.keys():
+        if client != connection:
+            client.sendall(message)
+
 def client_connect(connection, address):
+    clients[connection] = address
     while True:
         data = connection.recv(1024)
         if len(data) == 0:
              break
         print(f"Received: {data}")
 
-        connection.sendall(data)
-        print(f"Client disconnected: {address}")
+        broadcast(data, connection)
 
+    print(f"Client disconnected: {address}")
+    del clients[connection]
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.bind(server_address)
